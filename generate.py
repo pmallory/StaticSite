d94fd3c3afc361f3f4651628032398da0e962198
Philip Mallory <pmallory@altalang.com>
2012-02-15 22:07:03 -0500

    add content/tag parser, simplify tag syntax

--- generate.py
+++ generate.py
@@ -7,7 +7,22 @@ def render(file, template):
     template = os.path.join(template,'post.tmpl')
     with open(template) as template_file:
         template_string = Template(template_file.read())
-    print template_string.safe_substitute(title='tits')
+    print template_string.safe_substitute(parse_content(file))
+
+def parse_content(path):
+    content_dict = {}
+    with open(path) as raw_post:
+        for line in raw_post.readlines():
+            if line.startswith('#'):
+                current_tag = line.strip('#\n')
+                content_dict[current_tag]= ''
+            else:
+                content_dict[current_tag] += line
+
+    print content_dict
+    return content_dict
+
+        
 
 for root, dirs, files in os.walk(settings.content_path):
     for file in files:
