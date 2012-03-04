8b77de7a9c867fd0929fd90304f163c607052065
Philip Mallory <philip@pmallory.com>
2012-03-04 00:36:13 -0500

    Explanatory comments added

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
+# where to find templates
 template_path = './templates'
+
+# where to find content
 content_path = './content'
+
+# where to output the rendered site
 output_path = './www'
+
+# used if a content file doesn't specify a template
 default_template = './templates/default.tmpl'
 
